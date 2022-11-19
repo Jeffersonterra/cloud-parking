78ae610b2bb0569f021800e2f07f7f237d6cbd77
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-11-19 17:39:15 -0300

    Primeiro end point de pesquisa

--- src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
+++ src/main/java/one/digitalinnovation/parking/controller/mapper/ParkingMapper.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Component
-public class ParkingMapper {
+@Component 
+public class ParkingMapper{
 
     private static final ModelMapper MODEL_MAPPER = new ModelMapper();
 
